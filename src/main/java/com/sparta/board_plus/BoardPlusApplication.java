ea500cd13ec686958614015c8b714eebe22b0e5b
guxo2795 <guxo1211@gmail.com>
2023-12-16 19:07:59 +0900

    init commit

---
+++ src/main/java/com/sparta/board_plus/BoardPlusApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.board_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/board_plus/BoardPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.board_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
